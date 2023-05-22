a525d39187006a9006057911891cd21dbeca31bc
msestrem <msestrem@gmail.com>
2023-05-22 18:44:35 -0300

    ajuste3

--- produto_api.py
+++ produto_api.py
@@ -1,12 +1,24 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/calcular-produto/<float:quantidade>', methods=['GET'])
-def calcular_produto_endpoint(quantidade):
+
+@app.route('/calcular-produto', methods=['POST'])
+def calcular_produto_endpoint():
+    quantidade = request.json.get('quantidade')
+    if quantidade is None:
+        return jsonify({'error': 'Quantidade não fornecida'}), 400
+
+    try:
+        quantidade = float(quantidade)
+    except ValueError:
+        return jsonify({'error': 'Quantidade inválida'}), 400
+
     produto = quantidade * 1.68
     return jsonify({'resultado': produto})
 
+
 if __name__ == '__main__':
     app.run()
+
 
