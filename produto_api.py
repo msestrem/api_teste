326462e4f5ac228b8c84aa3a028c3bf3ca947a10
msestrem <msestrem@gmail.com>
2023-05-22 18:10:01 -0300

    ajuste1

--- produto_api.py
+++ produto_api.py
@@ -4,26 +4,17 @@ app = Flask(__name__)
 
 @app.route('/calcular-produto', methods=['POST'])
 def calcular_produto_endpoint():
-    quantidade = request.json['quantidade']
+    quantidade = request.json.get('quantidade')
+    if quantidade is None:
+        return jsonify({'error': 'Quantidade não fornecida'}), 400
+
+    try:
+        quantidade = float(quantidade)
+    except ValueError:
+        return jsonify({'error': 'Quantidade inválida'}), 400
+
     produto = quantidade * 1.68
     return jsonify({'resultado': produto})
 
 if __name__ == '__main__':
     app.run()
-
-
-
-
-
-def calcular_produto(quantidade):
-    produto = quantidade * 1.68
-    return produto
-
-# Solicita ao usuário a quantidade
-quantidade = int(input("Digite a quantidade: "))
-
-# Chama a função para calcular o produto
-resultado = calcular_produto(quantidade)
-
-# Imprime o resultado
-print("O produto da quantidade por 1.68 é:", resultado)
