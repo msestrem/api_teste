1c86d0188c9ed4e468d90b35635f942c83e35ab5
msestrem <msestrem@gmail.com>
2023-05-22 19:09:30 -0300

    ajuste4

--- produto_api.py
+++ produto_api.py
@@ -5,16 +5,31 @@ app = Flask(__name__)
 
 @app.route('/calcular-produto', methods=['POST'])
 def calcular_produto_endpoint():
-    quantidade = request.json.get('quantidade')
+    quantidade = request.json.get('cpfCnpj', 'quantidade')
     if quantidade is None:
-        return jsonify({'error': 'Quantidade não fornecida'}), 400
+        error = jsonify({'error': 'Quantidade não fornecida'}), 400
 
     try:
         quantidade = float(quantidade)
     except ValueError:
-        return jsonify({'error': 'Quantidade inválida'}), 400
+        return error
+    preco_unit = [{'cpfCnpj': '40238802000159', 'preco': float(0.58)},
+                   {'cpfCnpj': '10423979000164', 'preco': float(0.62)},
+                   {'cpfCnpj': '37789234000170', 'preco': float(0.88)},
+                   {'cpfCnpj': '14991918000154', 'preco': float(0.83)},
+                   {'cpfCnpj': '40988513000177', 'preco': float(0.45)},
+                   {'cpfCnpj': '44765328000174', 'preco': float(0.29)}]
+    for item in preco_unit:
+        if item['cpfCnpj'] == cpfCnpj:
+            preco = item['preco']
+            break
+    else:
+        # Se não encontrar, define o valor do preço como None ou algum valor padrão
+        preco = None
 
-    produto = quantidade * 1.68
+    # Imprime o valor do preço
+    print(preco)
+    produto = quantidade * preco
     return jsonify({'resultado': produto})
 
 
